020a0e6c49a85353dd26968314bdb1acb8228271
DanilBond <75364237+DanilBond@users.noreply.github.com>
2026-07-17 01:39:24 +0900

    Added the ability to close tabs.

--- Assets/Scripts/Unity/UI/GamePanelTabs.cs
+++ Assets/Scripts/Unity/UI/GamePanelTabs.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,7 @@ namespace Azulon.Unity.UI
 {
     public enum GameSidePanel
     {
+        None = -1,
         Collection = 0,
         Inventory = 1,
         Quests = 2
@@ -21,7 +23,9 @@ namespace Azulon.Unity.UI
         [SerializeField] private GameObject _inventoryPanel;
         [SerializeField] private GameObject _questsPanel;
 
-        public GameSidePanel CurrentPanel { get; private set; }
+        public GameSidePanel CurrentPanel { get; private set; } = GameSidePanel.None;
+
+        public bool IsAnyPanelOpen => CurrentPanel != GameSidePanel.None;
 
         private void Awake()
         {
@@ -32,6 +36,7 @@ namespace Azulon.Unity.UI
                 return;
             }
 
+            _collectionToggle.group.allowSwitchOff = true;
             _collectionToggle.onValueChanged.AddListener(HandleCollectionChanged);
             _inventoryToggle.onValueChanged.AddListener(HandleInventoryChanged);
             _questsToggle.onValueChanged.AddListener(HandleQuestsChanged);
@@ -58,6 +63,29 @@ namespace Azulon.Unity.UI
 
         public void Show(GameSidePanel panel)
         {
+            if (panel == GameSidePanel.None)
+            {
+                Close();
+                return;
+            }
+
+            if (panel != GameSidePanel.Collection &&
+                panel != GameSidePanel.Inventory &&
+                panel != GameSidePanel.Quests)
+            {
+                throw new ArgumentOutOfRangeException(nameof(panel), panel, "Unknown side panel.");
+            }
+
+            ApplyState(panel);
+        }
+
+        public void Close()
+        {
+            ApplyState(GameSidePanel.None);
+        }
+
+        private void ApplyState(GameSidePanel panel)
+        {
             CurrentPanel = panel;
             _collectionPanel.SetActive(panel == GameSidePanel.Collection);
             _inventoryPanel.SetActive(panel == GameSidePanel.Inventory);
@@ -80,32 +108,59 @@ namespace Azulon.Unity.UI
                 return false;
             }
 
+            var toggleGroup = _collectionToggle.group;
+            if (toggleGroup == null ||
+                _inventoryToggle.group != toggleGroup ||
+                _questsToggle.group != toggleGroup)
+            {
+                error = $"Game panel tabs '{name}' must use the same ToggleGroup.";
+                return false;
+            }
+
+            if (!Enum.IsDefined(typeof(GameSidePanel), _initialPanel))
+            {
+                error = $"Game panel tabs '{name}' have an unknown initial panel.";
+                return false;
+            }
+
             error = null;
             return true;
         }
 
         private void HandleCollectionChanged(bool isOn)
         {
-            if (isOn)
-            {
-                Show(GameSidePanel.Collection);
-            }
+            HandleToggleChanged(GameSidePanel.Collection, isOn);
         }
 
         private void HandleInventoryChanged(bool isOn)
         {
-            if (isOn)
-            {
-                Show(GameSidePanel.Inventory);
-            }
+            HandleToggleChanged(GameSidePanel.Inventory, isOn);
         }
 
         private void HandleQuestsChanged(bool isOn)
         {
+            HandleToggleChanged(GameSidePanel.Quests, isOn);
+        }
+
+        private void HandleToggleChanged(GameSidePanel panel, bool isOn)
+        {
             if (isOn)
             {
-                Show(GameSidePanel.Quests);
+                Show(panel);
+                return;
             }
+
+            if (CurrentPanel == panel && !AreAnyTogglesOn())
+            {
+                Close();
+            }
+        }
+
+        private bool AreAnyTogglesOn()
+        {
+            return _collectionToggle.isOn ||
+                   _inventoryToggle.isOn ||
+                   _questsToggle.isOn;
         }
     }
 }
